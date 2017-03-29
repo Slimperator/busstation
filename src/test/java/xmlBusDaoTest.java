c3387a8786fd97bd2c8ae846226870a4f1666a37
Slimperator <slimperator@gmail.com>
2017-03-29 15:19:25 +0300

    add xmlDao test

---
+++ src/test/java/xmlBusDaoTest.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Владимир on 29.03.2017.
+ */
+import com.busstation.server.storage.dao.impl.XmlBusDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ContextConfiguration(locations = {
+        "file:src/main/webapp/WEB-INF/applicationContext.xml",
+        "file:src/main/webapp/WEB-INF/classes/action-servlet.xml",})
+public class xmlBusDaoTest {
+    @Autowired
+    XmlBusDao dao;
+
+    @Test
+    public  void testCount() {
+        Integer test = dao.getBusCount();
+        assertTrue("Count yep", test!=null);
+    }
+    @Test
+    public  void testGetBus() {
+        List<String> testList = dao.getBus(1);
+        assertTrue("Get Bus Yep", (testList != null && testList.size()!=0));
+    }
+}
